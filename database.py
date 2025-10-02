2ed86b52d18c0998373cfd745eb499577aeca73b
karollept <karoliina.lepisto@metropolia.fi>
2025-10-03 00:56:59 +0300

    tehdään asia

--- database.py
+++ database.py
@@ -3,22 +3,15 @@ yhteys = mysql.connector.connect(
     host='localhost',
     port= 3306,
     database='lk_approt',
-    user='kartsu',
-    password='black_rose',
+    user='user',          #your password
+    password='password',  #your password
     autocommit=True
     )
 cursor = yhteys.cursor()
-# maria MD käskyjä
-# update airport set minigame_id =
-# (select id from minigame where name = "Numeron arvaus") where ident = "EFHK";
+maria MD käskyjä
+update airport set minigame_id =
+(select id from minigame where name = "Numeron arvaus") where ident = "EFHK";
 
-#cursor.execute("INSERT INTO player (budjet) VALUES (%s)",
-              # ("100",) where name = "karo")
-cursor.execute("CREATE TABLE IF NOT EXISTS connection ("
-               "id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,"
-               "ident1 VARCHAR(40) NOT NULL,"
-               "ident2 VARCHAR(40) NOT NULL,"
-               "FOREIGN KEY(ident1) REFERENCES airport(ident)"
-               "FOREIGN KEY(ident2) REFERENCES airport(ident)"
-            )
+cursor.execute("INSERT INTO player (budjet) VALUES (%s)",
+              ("100",) where name = "karo")
 
