3ca5e063e11fd433aee46918a6207a942d224096
Jonttu1000 <jonatangomesma@gmail.com>
2025-10-04 21:45:37 +0300

    update

--- Jonatan/main.py
+++ Jonatan/main.py
@@ -45,62 +45,20 @@ def is_name_taken(nimi):
 
     return tulos[0] if len(tulos) > 0 else None
 
-#Etsii lento yhteydet
-def flight_path(location):
-    c= yhteys.cursor()
-    c.execute("SELECT kenttä.nimi FROM kenttä "
-              "INNER JOIN yhteys ON kenttä.id=yhteys.yhteys_id "
-              "WHERE yhteys.kenttä_id=(%s)",
-              (location,))
 
-    tulos = c.fetchall()
-    c.close()
-
-    print("Valitse minne haluat lentää:")
-    for i in range(len(tulos)):
-        print(f"{i+1}.{tulos[i][0]}")
-    return tulos
-
-
-def flight_choise(options, loop):
-
-    while loop ==True:
-        choise = input("")
-        if choise not in ("1", "2", "3"):
-            print("Syötä kenttään vain 1 , 2 tai 3")
-        else:
-            loop = False
-
-    airport_choise = options[int(choise)-1]
-    return airport_choise
-
-def chosen_airport_id(airport):
-    c = yhteys.cursor()
-
-    c.execute("SELECT kenttä.id FROM kenttä "
-              "WHERE kenttä.nimi = (%s)",
-              airport)
-    tulos = c.fetchone()
-    c.close()
-    return tulos[0]
-
-def_loop = True
 
 player = None
 while player == None:
-    player = choose_player()
-
-print(player)
-
-
-location = 1
-main_loop = True
-while main_loop == True:
-
-    yhteydet=flight_path(location)
-    valittu_kenttä=flight_choise(yhteydet, def_loop)
-
-    location=chosen_airport_id(valittu_kenttä)
+    player = choose_player()  #pelinimen valinta
 
 
 
+
+import wordle_peli
+
+green = "\033[32m"
+yellow = "\033[33m"
+reset = "\033[0m"
+loop_wordle = 0
+
+win_wordle=wordle_peli.wordle(loop_wordle, green, yellow, reset)
