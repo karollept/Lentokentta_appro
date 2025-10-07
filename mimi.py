0f3d07ce45444b6ac7dd75f11898838947966bb4
karollept <karoliina.lepisto@metropolia.fi>
2025-10-07 16:17:58 +0300

    tehdään asia

---
+++ mimi.py
@@ -0,0 +1,34 @@
+import mysql.connector
+
+
+yhteys = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password = "salasana",
+    autocommit = True,
+    db = "lk_approt",
+    port = 3306
+)
+cursor = yhteys.cursor()
+
+def highscore():
+    sql = """
+    SELECT 
+        screen_name AS "Pelaaja",
+        COUNT(token_id) AS 'Merkkien määrä'
+    FROM accomplishment
+    JOIN player ON accomplishment.player_id = player.id
+    GROUP BY player.id
+    ORDER BY 'Merkkien määrä' DESC;
+    """
+    cursor.execute(sql)
+    tulokset = cursor.fetchall()
+
+    print("🏆 Highscore – kerätyt merkit")
+    print("-" * 40)
+
+    for pelaaja, maara in tulokset:
+        print(f"{pelaaja:15} | {maara} merkkiä")
+
+    cursor.close()
+highscore()
