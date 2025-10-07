85fb4159b310c7dcaf4c609969d2cb7c909c2da3
karollept <karoliina.lepisto@metropolia.fi>
2025-10-07 15:59:56 +0300

    tehdään asia

--- game.py
+++ game.py
@@ -113,3 +113,25 @@ while budget > 0:
     budget, location = update_player(choise, player, budget)
 
 print("Olet kuluttanut opintolainan loppuun")
+
+def highscore():
+    sql = """
+    SELECT 
+        screen_name AS "Pelaaja",
+        COUNT(token_id) AS 'Merkkien määrä'
+    FROM accomplishment
+    JOIN player ON accomplishment.player_id = player.id
+    GROUP BY player.id
+    ORDER BY 'Merkkien määrä' DESC;
+    """
+    cursor.execute(sql)
+    tulokset = cursor.fetchall()
+
+    print("🏆 Highscore – kerätyt merkit")
+    print("-" * 40)
+
+    for pelaaja, maara in tulokset:
+        print(f"{pelaaja:15} | {maara} merkkiä")
+
+    cursor.close()
+highscore()
