e2477e32e31a323dbd192a14530e9c91580488ff
karollept <karoliina.lepisto@metropolia.fi>
2025-10-08 18:38:34 +0300

    new

--- game.py
+++ game.py
@@ -399,7 +399,7 @@ def hirsipuu ():
     loop_hirsipuu = 0
     arvatut_kirjaimet_hirsipuu = []
     win_hirsipuu = False
-
+    win = True
 
 
     difficulty_hirsipuu = difficulty_hirsipuu_def()
@@ -426,12 +426,13 @@ def hirsipuu ():
                   f"Arvauksi jäljellä :{10 - loop_hirsipuu}")
 
     if win_hirsipuu == True:
-        voitto = True
+        win = True
         print("Voitit pelin!")
     else:
         print(f"Hävisit pelin:(\n"
               f"Oikea vastaus oli {vastaus_hirsipuu}")
-        voitto = False
+        win = False
+    return win
 
 
 def init_db():
@@ -531,50 +532,47 @@ if __name__ == "__main__":
     result, player_hand, dealer_hand = play_game()
     record_result(result, player_hand, dealer_hand)
 
-import random
+def matikkavisa():
 
-# KYSYMYKSET JA VASTAUKSET
-kysymykset = {
-    "5 + 3": 8,
-    "9 - 4": 5,
-    "7 * 2": 14,
-    "12 / 3": 4,
-    "8 + 6": 14,
-    "15 - 7": 8,
-    "4 * 5": 20,
-    "20 / 4": 5,
-    "10 + 9": 19,
-    "14 - 6": 8,
-    "6 * 3": 18,
-    "18 / 2": 9,
-    "11 + 8": 19,
-    "13 - 9": 4,
-    "9 * 4": 36,
-    "24 / 6": 4,
-    "16 + 7": 23,
-    "25 - 12": 13,
-    "5 * 8": 40,
-    "30 / 5": 6
-}
+    kysymykset = {
+        "5 + 3": 8,
+        "9 - 4": 5,
+        "7 * 2": 14,
+        "12 / 3": 4,
+        "8 + 6": 14,
+        "15 - 7": 8,
+        "4 * 5": 20,
+        "20 / 4": 5,
+        "10 + 9": 19,
+        "14 - 6": 8,
+        "6 * 3": 18,
+        "18 / 2": 9,
+        "11 + 8": 19,
+        "13 - 9": 4,
+        "9 * 4": 36,
+        "24 / 6": 4,
+        "16 + 7": 23,
+        "25 - 12": 13,
+        "5 * 8": 40,
+        "30 / 5": 6}
 
-# 5 RANDOM KYSYMYSTÄ
-valitut_kysymykset = random.sample(list(kysymykset.items()), 5)
+    valitut_kysymykset = random.sample(list(kysymykset.items()), 5)
 
-print("Tervetuloa pelaamaan! Vastaa viiteen kysymykseen oikein voittaaksesi.")
-print("Jos vastaat väärin, peli loppuu heti.")
+    print("Tervetuloa pelaamaan! Vastaa viiteen kysymykseen oikein voittaaksesi.")
+    print("Jos vastaat väärin, peli loppuu heti.")
 
 # PELI
-for kysymys, vastaus in valitut_kysymykset:
-    try:
-        user_input = float(input(f"{kysymys} = "))
-    except ValueError:
-        print("Virheellinen syöte.")
-        break
-    if user_input != vastaus:
-        print(f"Väärin! Oikea vastaus on {vastaus}. Hävisit pelin.")
-        break
-else:
-    print("Onneksi olkoon! Vastasit kaikki oikein ja voitit pelin.")
+    for kysymys, vastaus in valitut_kysymykset:
+        try:
+            user_input = float(input(f"{kysymys} = "))
+        except ValueError:
+            print("Virheellinen syöte.")
+            break
+        if user_input != vastaus:
+            print(f"Väärin! Oikea vastaus on {vastaus}. Hävisit pelin.")
+            break
+    else:
+        print("Onneksi olkoon! Vastasit kaikki oikein ja voitit pelin.")
 
 
 def tarina(location):
@@ -625,7 +623,7 @@ while budget > 0:
 
     play_game()
     
-    if voitto == True:
+    if win == True:
         token(location)
 
 print("Olet kuluttanut opintolainan loppuun")
