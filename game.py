c7fd0f76929797215990e89b4cd027295256fec2
karollept <karoliina.lepisto@metropolia.fi>
2025-10-08 14:47:37 +0300

    new

--- game.py
+++ game.py
@@ -153,6 +153,31 @@ def kivi_sakset_paperi():
     else:
         print("Peli päättyi tasapeliin!")
         won = False
+    return won
+
+def numeron_arvauspeli():
+    print("Tervetuloa numeron arvauspeliin!")
+    print("Ajattelen numeroa 1 ja 9 välillä...")
+
+    oikea_numero = random.randint(1, 9)
+    arvaukset = 0
+
+    while True:
+        try:
+            arvaus = int(input("Arvaa numero: "))
+            arvaukset += 1
+
+            if arvaus < oikea_numero:
+                print("Liian pieni! Yritä uudelleen.")
+            elif arvaus > oikea_numero:
+                print("Liian suuri! Yritä uudelleen.")
+            else:
+                print(f"Onneksi olkoon! Arvasit oikein numeron {oikea_numero} {arvaukset} yrityksellä.")
+                won = True
+                break
+        except ValueError:
+            print("Ole hyvä ja syötä kokonaisluku.")
+    return won
 
 location = "EFHK"
 budget = 20000
